c8f8548e9b2494794577e8db131e3b58d23437a8
Ariel Zhen <zhenpingping@live.com>
2018-08-22 00:39:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    if  @user = User.find_by(username: params["username"], password: params["password"])
+    @user = User.find_by(username: params["username"], password: params["password"])
+    if @user 
       session[:user_id] = @user.id
       redirect to '/account'
     else
